9a6d95928c1d21f1971f997f933336c66d381711
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-08 15:58:21 +0900

    Controller, views and routers working. Basic functionality OK.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    @letters << ['a', 'e', 'i', 'o', 'u'][rand(5)] #Always adds a vowel first.
+    11.times{ @letters << ('a'..'z').to_a[rand(26)] }
+    @letters.shuffle!
+  end
+
+  def score
+    available_array = params[:available_letters].split(" ")
+    user_array = params[:answer].split("")
+    @check = check_dictionary(params[:answer])
+    if compare_arrays(available_array, user_array) && @check['found'] == true
+      @result = 'Well done'
+    elsif compare_arrays(available_array, user_array) == false
+      @result = 'Try again'
+    elsif @check['found'] == false
+      @result = 'That is not an English word'
+    end
+  end
+
+  private
+
+  def compare_arrays(available_array, user_array)
+    user_array.all? do |letter|
+      available_array.count(letter) >= user_array.count(letter)
+    end
+  end
+
+  def check_dictionary(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    dictionary_hash = URI.open(url).read
+    JSON.parse(dictionary_hash)
+  end
+end
